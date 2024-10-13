422fb1e13fbfdad03a712203bb1e82b83be2582d
massixdio <d.massandje14@gmail.com>
2024-10-13 10:49:23 +0000

    first commit

---
+++ app/src/main/java/com/example/quadrant/MainActivity.kt
@@ -0,0 +1,143 @@
+ package com.example.quadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quadrant.ui.theme.QuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuadrantTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Quardrant(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Quardrant(name: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 10.dp)
+    ) {
+        Row (
+        ){
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color(0xFFEADDFF))
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = "Text composable",
+                            fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Displays text and follows the recommended Material Design guidelines.",
+                            fontSize = 16.sp,
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color(0xFFD0BCFF))
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = "Image composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Creates a composable that lays out and draws a given Painter class object.",
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+        Row(
+        ){
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color(0xFFB69DF8))
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = "Row composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a horizontal sequence.\n",
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color(0xFFF6EDFF))
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = "Column composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a vertical sequence.",
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuadrantTheme {
+        Quardrant("Android")
+    }
+}
